181fe7958a5961fb23feedea47764d661ee319fd
Arrorn <drkmcavoy@gmail.com>
2012-04-04 07:39:06 -0400

    Finished LoadOrder and ModuleDescriptionFile
    Signed-off-by: Arrorn <drkmcavoy@gmail.com>

--- app/com/aliashost/WebMaster/module/LoadOrder.scala
+++ app/com/aliashost/WebMaster/module/LoadOrder.scala
@@ -2,6 +2,7 @@ package com.aliashost.WebMaster.module
 
 object LoadOrder extends Enumeration {
 	type LoadOrder = Value
-	val STARTUP = Value
+	val STARTUP = Value("STARTUP")
+	
 
 }
--- app/com/aliashost/WebMaster/module/ModuleDescriptionFile.scala
+++ app/com/aliashost/WebMaster/module/ModuleDescriptionFile.scala
@@ -25,6 +25,7 @@
  */
 
 package com.aliashost.WebMaster.module
+
 import com.aliashost.WebMaster.exception.InvalidDescriptionFileException
 import com.aliashost.WebMaster.module.LoadOrder
 
@@ -34,8 +35,8 @@ class ModuleDescriptionFile(private var name : String, private var version : Str
 	private var author : String = null
 	private var authors : List[String] = null
 	private var website : String = null
-	private var reload : Boolean = false
-	private var load : LoadOrder = LoadOrder.STARTUP
+	private var reload = false
+	private var load = LoadOrder.STARTUP
 	private var depends : List[String] = null
 	private var softdepends : List[String] = null
 	private var fullname : String = new StringBuilder().append(name).append(" v").append(version).toString()
@@ -43,13 +44,196 @@ class ModuleDescriptionFile(private var name : String, private var version : Str
 	
 	@throws(classOf[InvalidDescriptionFileException])
 	private def load(map : Map[String, Object]) : Unit = {
+		
 		try{
 			name = map.get("name").asInstanceOf[String]
 			
 			if(!name.matches("^[A-Za-z0-9 _.-]+$")){
-				throw new InvalidDescriptionFileException("The field 'name' in )
+				throw new InvalidDescriptionFileException("The field 'name' in module.xml contains invalid characters.")
+			}
+			
+			if(name.toLowerCase().contains("webmaster")){
+				throw new InvalidDescriptionFileException(new StringBuilder().append("The plugin '").append(name).append("' has WebMaster in the name. This is not allowed.").toString)
+			}
+			
+		}
+		catch{
+			case ex : NullPointerException => throw new InvalidDescriptionFileException("The field 'name' is not defined in the module.xml!", ex)
+			case ex : ClassCastException => throw new InvalidDescriptionFileException("The field 'name' is of the wrong type in the module.xml!", ex)
+		}
+		
+		try{
+			
+			main = map.get("main").asInstanceOf[String]
+			
+			if(!isOfficialPlugin(main)){
+				if(main.toLowerCase().startsWith("com.aliashost.")){
+					throw new InvalidDescriptionFileException("The use of the namespace 'com.aliashost' is not permitted.")
+				}
+				else if(main.toLowerCase().startsWith("net.aliashost.")){
+					throw new InvalidDescriptionFileException("The use of the namespace 'net.aliashost' is not permitted.")
+				}
+				else if(main.toLowerCase().startsWith("org.aliashost.")){
+					throw new InvalidDescriptionFileException("The use of the namespace 'org.aliashost' is not permitted.")
+				}
+				else if(main.toLowerCase().startsWith("com.amadecia.")){
+					throw new InvalidDescriptionFileException("The use of the namespace 'com.amadecia' is not permitted.")
+				}
+				else if(main.toLowerCase().startsWith("com.arrorn.")){
+					throw new InvalidDescriptionFileException("The use of the namespace 'com.arrorn' is not permitted.")
+				}
 			}
 		}
+		catch{
+			case ex : NullPointerException => throw new InvalidDescriptionFileException("The field 'main' is not defined in the module.xml!", ex)
+			case ex : ClassCastException => throw new InvalidDescriptionFileException("The field 'main' is of the wrong type in the module.xml!", ex)
+		}
+		
+		try{
+			version = map.get("version").toString()
+		}
+		catch{
+			case ex : NullPointerException => throw new InvalidDescriptionFileException("The field 'version' is not defined in the module.xml!", ex)
+			case ex : ClassCastException => throw new InvalidDescriptionFileException("The field 'version' is of the wrong type in the module.xml!", ex)
+		}
+		
+		fullname = new StringBuilder().append(name).append(" v").append(version).toString()
+		
+		if(map.contains("author")){
+			try{
+				author = map.get("author").asInstanceOf[String]
+			}
+			catch{
+				case ex : ClassCastException => throw new InvalidDescriptionFileException("The field 'author' is of the wrong type in the module.xml!", ex)
+			}
+		}
+		
+		if(map.contains("authors")){
+			try{
+				authors = map.get("authors").asInstanceOf[List[String]]
+			}
+			catch{
+				case ex : ClassCastException => throw new InvalidDescriptionFileException("The field 'authors' is of the wrong type in the module.xml!", ex)
+			}
+		}
+		
+		if(map.contains("depends")){
+			try{
+				depends = map.get("depends").asInstanceOf[List[String]]
+			}
+			catch{
+				case ex : ClassCastException => throw new InvalidDescriptionFileException("The field 'depends' is of the wrong type in the module.xml!", ex)
+			}
+		}
+		
+		if(map.contains("softdepends")){
+			try{
+				softdepends = map.get("softdepends").asInstanceOf[List[String]]
+			}
+			catch{
+				case ex : ClassCastException => throw new InvalidDescriptionFileException("The field 'softdepends' is of the wrong type in the module.xml!", ex)
+			}
+		}
+		
+		if(map.contains("description")){
+			try{
+				description = map.get("description").asInstanceOf[String]
+			}
+			catch{
+				case ex : ClassCastException => throw new InvalidDescriptionFileException("The field 'description' is of the wrong type in the module.xml!", ex)
+			}
+		}
+		
+		if(map.contains("load")){
+			try{
+				load = LoadOrder.withName(map.get("description").asInstanceOf[String].toUpperCase())
+			}
+			catch{
+				case ex : ClassCastException => throw new InvalidDescriptionFileException("The field 'load' is of the wrong type in the module.xml!", ex)
+			}
+		}
+		
+		if(map.contains("reload")){
+			try{
+				reload = map.get("reload").asInstanceOf[Boolean]
+			}
+			catch{
+				case ex : ClassCastException => throw new InvalidDescriptionFileException("The field 'reload' is of the wrong type in the module.xml!", ex)
+			}
+		}
+		
+		if(map.contains("website")){
+			try{
+				website = map.get("website").asInstanceOf[String]
+			}
+			catch{
+				case ex : ClassCastException => throw new InvalidDescriptionFileException("The field 'website' is of the wrong type in the module.xml!", ex)
+			}
+		}
+		
+		if(map.contains("protocol")){
+			try{
+				protocol = map.get("protocol").asInstanceOf[String]
+			}
+			catch{
+				case ex : ClassCastException => throw new InvalidDescriptionFileException("The field 'protocol' is of the wrong type in the module.xml!", ex)
+			}
+		}	
+	}
+	
+	private def isOfficialPlugin(namespace : String) : Boolean = {
+		return false
+	}
+	
+	def getName() : String = {
+		return name;
+	}
+
+	def getVersion() : String = {
+		return version;
+	}
+
+	def getDescription() : String = {
+		return description;
+	}
+
+	def getAuthor() : String = {
+		return author;
+	}
+
+	def getAuthors() : List[String] = {
+		return authors;
+	}
+
+	def getWebsite() : String = {
+		return website;
+	}
+
+	def allowsReload() : Boolean = {
+		return reload;
+	}
+
+	def getLoad() : LoadOrder = {
+		return load;
+	}
+
+	def getMain() : String = {
+		return main;
+	}
+	
+	def getDepends() : List[String] = {
+		return depends;
+	}
+	
+	def getSoftDepends() : List[String] = {
+		return softdepends;
+	}
+	def getFullName() : String = {
+		return fullname;
+	}
+	
+	def getProtocol() : String = {
+		return protocol;
 	}
   
 }
